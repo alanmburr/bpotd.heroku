1574a0fc60c4cc8ba157a7f1ba2d29a1b481282c
ab25 <54408289+wackyblackie@users.noreply.github.com>
2021-03-29 19:04:44 -0400

    Add files via upload

--- docs/cookie.js
+++ docs/cookie.js
@@ -3,9 +3,9 @@ function setCookie(cname,cvalue,exdays) {
     d.setTime(d.getTime() + (exdays*24*60*60*1000));
     var expires = "expires=" + d.toGMTString();
     document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
-  }
+}
   
-  function getCookie(cname) {
+function getCookie(cname) {
     var name = cname + "=";
     var decodedCookie = decodeURIComponent(document.cookie);
     var ca = decodedCookie.split(';');
@@ -19,13 +19,13 @@ function setCookie(cname,cvalue,exdays) {
       }
     }
     return "";
-  }
+}
   
-  function checkCookie() {
+function checkCookie() {
     var accepted = getCookie("cookiesAccepted");
     if (accepted != "") {
       document.getElementById('right').style.display = "none";
     } else {
         document.getElementById('right').style.display = "initial";
     }
-  }
+}
--- docs/ie.js
+++ docs/ie.js
@@ -1,14 +1,16 @@
-if (navigator.userAgent.indexOf('MSIE')!==-1 || navigator.appVersion.indexOf('Trident/') > -1){
-    var mywin = window.open("", "_self", "", true);
-    document.getElementById("left").style.display = "none"; document.getElementById("right").style.display = "none";
-    mywin.document.write(
-        "<title>Incompatble Browser</title><meta charset=\"utf-8\">"+
-        "<link href=\"https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css\" rel=\"stylesheet\"/>" +
-        "<center><h1>Your browser is not compatible with this webpage.</h1>" +
-        "<p>Try one of these browsers: <a href=\"https://firefox.com\">Mozilla Firefox</a>, " +
-        "<a href=\"https://chrome.com\">Google Chrome</a>, <a href=\"https://microsoft.com/edge\">Microsoft Edge</a></p>" +
-        "<hr><code style=\""+
-        "background-color: lightgrey; border: 1px solid black; border-radius: 6px; padding: 4px 8px; font-size: 12px; font-family: monospace;" +
-        "\">" + navigator.userAgent +
-        "</code></center>");
+function stopIEbrowsing(ifNotIE) {
+    if (navigator.userAgent.indexOf('MSIE')!==-1 || navigator.appVersion.indexOf('Trident/') > -1){
+        document.body.innerHTML = "<div id=\"incompatibleBrowserDiv\"><title>Incompatble Browser</title><meta charset=\"utf-8\">"+
+            "<link href=\"https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css\" rel=\"stylesheet\"/>" +
+            "<center><h1>Your browser is not compatible with this webpage.</h1>" +
+            "<p>Try one of these browsers: <a href=\"https://firefox.com\">Mozilla Firefox</a>, " +
+            "<a href=\"https://chrome.com\">Google Chrome</a>, <a href=\"https://microsoft.com/edge\">Microsoft Edge</a></p>" +
+            "<hr><code style=\""+
+            "background-color: lightgrey; border: 1px solid black; border-radius: 6px; padding: 4px 8px; font-size: 12px; font-family: monospace;" +
+            "\">" + navigator.userAgent +
+            "</code></center></div>";
+        return false; 
+    }
 }
+
+document.onload = stopIEbrowsing(); 
