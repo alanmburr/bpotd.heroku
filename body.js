f182086beaa5408bdaa5926160e83c9fb9d0eb24
ab25 <wackyblackie-page-feedback@outlook.com>
2021-04-04 10:47:33 -0400

    Add section to remove elements in IE

--- body.js
+++ body.js
@@ -50,6 +50,11 @@ if (window.innerWidth <= 1320) {
     document.getElementById('theImg').setAttribute("width", imgWidth);
     declareThese();
 }
+if (navigator.userAgent.indexOf('MSIE')!==-1 || navigator.appVersion.indexOf('Trident/') > -1){
+    document.getElementById("stgs").style.display = "none";
+    document.getElementById("openInNewTab").style.display = "none";
+    document.getElementById("downloadPhoto").style.display = "none";
+}
 
 var readmore = baseUrl;
 var readmoreLocation = Number(readmore.search("class=\"learn_more\""));
