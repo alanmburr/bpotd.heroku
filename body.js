0ce1ffc15710843a7d50d2dcf5b131a09b31299a
ab25 <wackyblackie-page-feedback@outlook.com>
2021-04-04 10:50:16 -0400

    Fix IE display="none" problem

--- body.js
+++ body.js
@@ -52,8 +52,8 @@ if (window.innerWidth <= 1320) {
 }
 if (navigator.userAgent.indexOf('MSIE')!==-1 || navigator.appVersion.indexOf('Trident/') > -1){
     document.getElementById("stgs").style.display = "none";
-    document.getElementById("openInNewTab").style.display = "none";
-    document.getElementById("downloadPhoto").style.display = "none";
+    document.getElementById("ieprobOIINT").style.display = "none";
+    document.getElementById("ieprobDP").style.display = "none";
 }
 
 var readmore = baseUrl;
