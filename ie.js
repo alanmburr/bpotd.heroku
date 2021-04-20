71818ad96b95e1cf43bb086c604f29a09d3c23cf
ab25 <wackyblackie-page-feedback@outlook.com>
2021-04-19 20:27:29 -0400

    Update ie.js

--- ie.js
+++ ie.js
@@ -1,5 +1,3 @@
 if (navigator.userAgent.indexOf('MSIE')!==-1 || navigator.appVersion.indexOf('Trident/') > -1){
-    document.getElementById("stgs").style.display = "none";
-    document.getElementById("openInNewTab").style.display = "none";
-    document.getElementById("downloadPhoto").style.display = "none";
+    location.replace("/ie/");
 }
